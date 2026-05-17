26bf0c9801d3ecd73efcec9084258f7448721cc9
fl4meh <3nazmathiel@gmail.com>
2026-05-18 00:51:30 +0800

    added pause gui

---
+++ script/ui_scripts/pause.gd
@@ -0,0 +1,23 @@
+extends Control
+
+func _process(_delta):
+	if not Input.is_action_just_pressed("ESC"):
+		return
+	elif visible:
+		visible = false
+		get_tree().paused = false
+	else:
+		visible = true
+		get_tree().paused = true
+
+func _on_continue_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+func _on_main_menu_pressed() -> void:
+	visible = false
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
